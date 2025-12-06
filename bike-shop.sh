2baccba3f825943908f756f3071a788eb134ec6d
chacaluigi <mariomachaca5477@gmail.com>
2025-12-05 21:29:41 -0500

    Save progress: 1474.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -95,6 +95,10 @@ RETURN_MENU() {
   read PHONE_NUMBER
   CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$PHONE_NUMBER'")
   # if not found
+  if [[ -z $CUSTOMER_ID ]]
+  then
+    # send to main menu 
+  fi
   # send to main menu
 }
 
